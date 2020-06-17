194f260d922c0f3a25bf298b4c3d1ff3c83307af
Lluís <lluis.revilla@gmail.com>
2020-06-17 13:16:55 +0200

    Improve final formatting of issues
    Also sta

--- R/issues.R
+++ R/issues.R
@@ -10,6 +10,42 @@
 #' issues <- get_issues("llrs/blogR")
 #' @importFrom gh gh
 get_issues <- function(repository) {
-    issues <- gh("/repos/:repo/issues", repo = repository, .limit = Inf)
+    issues <- gh("/repos/:repo/issues", repo = repository, .limit = Inf,
+                 state = "all",
+                 .send_headers = header)
     issues <- apply_class(issues, "issue")
+    issues$state <- unlist(issues$state, FALSE, FALSE)
+    issues$id <- unlist(issues$id, FALSE, FALSE)
+    issues$n_comments <- unlist(issues$n_comments, FALSE, FALSE)
+    issues$text <- unlist(issues$text, FALSE, FALSE)
+    issues$title <- unlist(issues$title, FALSE, FALSE)
+    issues$created <- convert_dates(unlist(issues$created, FALSE, FALSE))
+    issues$updated <- convert_dates(unlist(issues$updated, FALSE, FALSE))
+    issues
+}
+
+get_issue <- function(repository, issue) {
+    issues <- gh("/repos/:repo/issues/:issue", repo = repository,
+                 .limit = Inf, issue = issue,
+                 state = "all",
+                 .send_headers = header)
+    issues <- apply_class(issues, "issue")
+    issues$state <- unlist(issues$state, FALSE, FALSE)
+    issues$id <- unlist(issues$id, FALSE, FALSE)
+    issues$n_comments <- unlist(issues$n_comments, FALSE, FALSE)
+    issues$text <- unlist(issues$text, FALSE, FALSE)
+    issues$title <- unlist(issues$title, FALSE, FALSE)
+    issues$created <- convert_dates(unlist(issues$created, FALSE, FALSE))
+    issues$updated <- convert_dates(unlist(issues$updated, FALSE, FALSE))
+    issues
+}
+
+get_full_issues <- function(repository, issue) {
+    browser()
+    issue <- get_issue(repository, issue)
+    comments <- get_comments(repository, issue)
+    comments$type <- "comment"
+    events <- get_events(repository, issue)
+    events$type <- "event"
+
 }
