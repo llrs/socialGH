15b20d3bd211e2d91abfc3dfca740e2461788915
Lluís <lluis.revilla@gmail.com>
2020-06-18 09:33:11 +0200

    Improve user naming

--- R/tidy.R
+++ R/tidy.R
@@ -1,14 +1,15 @@
 
-comment <- function(x) {
-    l <- list(text = x$body, created = x$created_at, updated = x$updated_at,
-      association = x$author_association, user(x$user),
-      id = url2id(x$issue_url))
-    names(l)[names(l) == "user"] <- "commenter"
+user <- function(x, name = "user") {
+    l <- list(user = x$login, type = x$type, admin = x$site_admin)
+    names(l)[1] <- name
     l
 }
 
-user <- function(x) {
-    list(user = x$login, type = x$type, admin = x$site_admin)
+comment <- function(x) {
+    l <- list(text = x$body, created = x$created_at, updated = x$updated_at,
+      association = x$author_association, id = url2id(x$issue_url))
+    u <- user(x$user, "commenter")
+    c(l, u)
 }
 
 labels <- function(y) {
@@ -18,17 +19,20 @@ labels <- function(y) {
 url2id <- function(x) {
     strsplit(x, "/", fixed = TRUE)[[1]][8]
 }
+
 milestones <- function(x) {
     vapply(x, getElement, name = "milestone", character(1L))
 }
 
 assignees <- function(x) {
-    list(n_comments = x$comments, assigned = x$created_at, user(x))
+    l <- list(n_comments = x$comments, assigned = x$created_at)
+    u <- user(x, "assigner")
+    c(l, u)
 }
 
 issue <- function(x) {
 
-    list(assignees = simplify(x$assignees, assignees),
+    l <- list(assignees = simplify(x$assignees, assignees),
       assignee = simplify(x$assignee, user),
       label = labels(x$labels),
       state = x$state,
@@ -39,14 +43,16 @@ issue <- function(x) {
       created = x$created_at,
       updated = x$updated_at,
       association = x$author_association,
-      commenter = user(x$user),
       text = x$body,
       id = x$number)
+    u <- user(x$user, "poster")
+    c(l, u)
 }
 
 event <- function(x) {
     # browser()
-    event <- c(event = x$event, date = x$created_at, user(x$actor))
-    names(event)[names(event) == "user"] <- "trigger"
-    c(issue(x$issue), event)
+    i <- issue(x$issue)
+    l <- list(event = x$event, date = x$created_at)
+    u <- user(x$actor, "trigger")
+    c(i, l, u)
 }
