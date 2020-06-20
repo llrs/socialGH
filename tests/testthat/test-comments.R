320610b1482341e3483919761d3009fde85faa85
Lluís <lluis.revilla@gmail.com>
2020-06-20 15:03:06 +0200

    Increase testing

--- tests/testthat/test-comments.R
+++ tests/testthat/test-comments.R
@@ -1,5 +1,14 @@
-test_that("get_comments works", {
+test_that("multiple get_comments works", {
     skip_if_offline()
     expect_null(get_comments("llrs/eigen"))
     expect_s3_class(get_comments("llrs/blogR"), "data.frame")
+})
+
+test_that("get_comments works", {
+    skip_if_offline()
+    expect_error(get_comments("llrs/eigen", 1))
+    expect_error(gc <- get_comments("llrs/BioCor", 1), NA)
+    expect_equal(colnames(gc),
+                 c("text", "created", "updated", "association", "id", "commenter"))
+
 })
--- tests/testthat/test-events.R
+++ tests/testthat/test-events.R
@@ -2,3 +2,13 @@ test_that("get_events works", {
     skip_if_offline()
     expect_error(get_events("llrs/blogR", 1), NA)
 })
+
+test_that("multiple get_events works", {
+    skip_if_offline()
+    expect_error(ge <- get_events("llrs/blogR"), NA)
+    expect_equal(ncol(ge), 18)
+    expect_equal(colnames(ge),
+                 c("assignees", "assignee", "label", "state", "locked", "milestone",
+                   "n_comments", "title", "created", "updated", "association", "text",
+                   "id", "closer", "poster", "event", "date", "triggerer"))
+})
