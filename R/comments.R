cd5755e723c4083f1ce0d89632de76d4a5118bc2
Lluís <lluis.revilla@gmail.com>
2020-06-20 13:04:47 +0200

    Correct condition to query all the comments

--- R/comments.R
+++ R/comments.R
@@ -11,18 +11,26 @@
 #' get_comments("llrs/blogR", 10)
 get_comments <- function(repository, issue = NULL) {
     if (is.null(issue)) {
-        comments <- gh("/repos/:repo/issues/:issue/comments",
-                       repo = repository, issue = issue, .limit = Inf,
-                       .send_headers = header)}
-    else {
         comments <- gh("/repos/:repo/issues/comments", .limit = Inf,
                        repo = repository,
                        .send_headers = header)
+    } else {
+        comments <- gh("/repos/:repo/issues/:issue/comments",
+                       repo = repository, issue = issue, .limit = Inf,
+                       .send_headers = header)
     }
+
+    # No issues
+    if (length(comments) == 0) {
+        return(NULL)
+    }
+
     df <- simplify(comments, comment)
+    unlist_vec <- c("text", "association")
+    df[unlist_vec] <- lapply(df[unlist_vec], unlist,
+                             use.names = FALSE, recursive = FALSE)
     df$created <- convert_dates(df$created)
     df$updated <- convert_dates(df$updated)
-    df$admin <- as.logical(df$admin)
     df$id <- as.numeric(df$id)
     df
 }
