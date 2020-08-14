0dfd804e708baa46b6e52638052fbb233522752c
Lluís <lluis.revilla@gmail.com>
2020-08-14 13:49:28 +0200

    Check API

---
+++ R/meta.R
@@ -0,0 +1,15 @@
+
+#' Check API
+#'
+#' Check if the API is available and how many calls can you can use.
+#' @references [Help page](https://docs.github.com/en/rest/reference/rate-limit#understanding-your-rate-limit-status)
+#' @examples
+#' check_api()
+check_api <- function() {
+    v <- gh("/rate_limit")
+    s <- simplify2array(v$resources)
+    s <- as.data.frame(s, stringsAsFactors = FALSE)
+    s$rate <- rownames(s)
+    rownames(s) <- NULL
+    s[1:2, ]
+}
